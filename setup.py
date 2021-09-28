535b01894ea0a665141635cb5b504adafa55969b
Paul Harper <benekastah@gmail.com>
2021-09-28 14:04:21 -0700

    bump to v1.1.18

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="target-postgres",
-    version="1.1.17",
+    version="1.1.18",
     description="Singer.io target for Postgres",
     author="Statsbot",
     url="https://statsbot.co",
